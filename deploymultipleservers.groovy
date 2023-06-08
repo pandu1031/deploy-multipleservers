0c8360f8388de9709497fe2e599c87b71c2688c8
pandu1031 <chintupandu1031@gmail.com>
2023-06-08 11:46:32 +0530

    deploy pipeline script

---
+++ deploymultipleservers.groovy
@@ -0,0 +1,28 @@
+//Declarative pipeline
+pipeline{
+    agent any 
+    parameters {
+        string(name: 'SERVERIP', defaultValue: '', description: 'Enter server ip')
+        string(name: 'BUILDNUM', defaultValue: '', description: 'Enter Build number')
+        string(name: 'BRANCH', defaultValue: '', description: 'Enter branch name')
+}
+stages{
+    stage("Deploy in Multiple servers"){
+        steps{
+
+            sh '''
+            aws s3 cp s3://mamuu/Dheeraj/${BRANCH}/${BUILDNUM}/hello-${BUILDNUM}.war .
+            ls -l 
+            whoami
+            IFS=',' read -ra storevalue <<< "${SERVERIP}"
+            for ip in ${storevalue[@]}
+            do
+            echo "$ip"
+            scp -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem hello-${BUILDNUM}.war ec2-user@$ip:/var/lib/tomcat/webapps
+            ssh -o StrictHostKeyChecking=no -i /tmp/mamu1031.pem ec2-user@$ip "hostname"
+            done
+            '''
+        }
+      }
+    }
+  }
